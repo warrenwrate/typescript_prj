e015f63484faf997aa4e709b3f295114797a10ec
Warren Wrate <wawrate@gmail.com>
2024-09-28 18:22:31 -0700

    changed Items to fruits

--- api/python_api.py
+++ api/python_api.py
@@ -8,9 +8,9 @@ CORS(app)
 @app.route('/api/items', methods=['GET'])
 def get_items():
     items = [
-        {"id": 1, "name": "Item 1"},
-        {"id": 2, "name": "Item 2"},
-        {"id": 3, "name": "Item 3"}
+        {"id": 1, "name": "banana"},
+        {"id": 2, "name": "strawberry"},
+        {"id": 3, "name": "apple"}
     ]
     return jsonify(items)
 
